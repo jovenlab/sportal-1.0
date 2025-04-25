f91d3e718e7c93e72de973d500b481286be17bf9
cess-m <pbparages@up.edu.ph>
2025-04-25 09:29:06 +0800

    Restore DarkModeToggle next to Search bar in Navbar

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import Search from './Search'
 import UserMenu from './UserMenu'
 import {SafeUser} from "@/app/types"
 import Categories from './Categories';
-
+import DarkModeToggle from "../DarkModeToggle";
 
 interface NavbarProps{
     currentUser?: SafeUser | null;
@@ -20,18 +20,23 @@ const Navbar: React.FC<NavbarProps> = ({
         <div className='fixed w-full bg-white/80 dark:bg-zinc-900/80 backdrop-blur-md text-black dark:text-white z-10 shadow-sm'>
             <div className='py-4 border-b-[1px]'>
                 <Container>
-                    <div
-                        className='
-                            flex
-                            flex-row
-                            items-center
-                            justify-between
-                            gap-3
-                            md:gap-0
-                            '>
-                        <Logo/>  
-                        <Search/>
-                        <UserMenu currentUser={currentUser}/>
+                <div
+                    className='
+                        flex
+                        flex-row
+                        items-center
+                        justify-between
+                        gap-3
+                        md:gap-0
+                    '>
+                    <Logo />
+
+                    <div className="flex flex-row items-center gap-3">
+                        <Search />
+                        <DarkModeToggle />
+                    </div>
+
+                    <UserMenu currentUser={currentUser} />
                     </div>
                 </Container>
             </div>
